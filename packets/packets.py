82f0e7ade376c60c3279b83773efc71bbb9b012d
Santi GM <s.g.martin94@gmail.com>
2022-12-26 14:17:35 +0100

    Add base packet classes

---
+++ packets/packets.py
@@ -0,0 +1,68 @@
+import time 
+
+from constants import InternalKeys
+
+class InPacket:
+    def __init__(self, buffer: bytearray, xor_key: int = InternalKeys.XOR_RECIEVE):
+        self.decoded_buffer = self.xor_decrypt(packet_buffer=buffer, xor_key=xor_key)
+        self.blocks = self.decoded_buffer.split(" ")
+        self.ticks, self.packet_id = self.parse_packet_header(blocks=self.blocks)
+        self.blocks = self.blocks[2:]
+
+    def parse_packet_header(self, blocks: list) -> tuple:
+        header = (blocks[0], blocks[1])
+        try:
+            return tuple(int(x) for x in header)
+        except ValueError as e:
+            print(f"Cannot parse packet header: {e}")
+            return header
+
+    def xor_decrypt(self, packet_buffer: bytearray, xor_key: int) -> str:
+        # Decrypt the bytes with the xOrKey.
+        for i in range(len(packet_buffer)):
+            packet_buffer[i] = packet_buffer[i] ^ xor_key
+
+        # Return the decrypted packet
+        decoded_buffer = packet_buffer.decode("utf-8")
+        return decoded_buffer
+
+
+class OutPacket:
+    def __init__(self, packet_id: int, xor_key: int = InternalKeys.XOR_SEND):
+        self.packet_id = packet_id
+        self.ticks = int(time.time())
+        self.blocks = [self.ticks, self.packet_id]
+        self.xor_key = xor_key
+
+    def append(self, value):
+
+        if isinstance(value, bool):
+            value_to_append = 1 if value else 0
+        elif isinstance(value, str):
+            value_to_append = data.replace(" ", chr(0x1D))
+        else:
+            value_to_append = value
+        
+        self.blocks.append(value_to_append)
+
+    def fill(self, value, times:int) -> None:
+        self.blocks.extend([value] * times)
+
+    def xor_encrypt(self, packet:str) -> bytearray:
+        buffer = bytearray(packet)
+        for i in range(len(buffer)):
+            buffer[i] = buffer[i] ^ self.xor_key 
+        
+        return buffer
+
+
+    def build(self, encrypted: bool = True) -> bytearray | bytes:
+        # End of packet termination chr.
+        self.blocks.append(chr(0x0A)) 
+        full_packet = " ".join(str(block) for block in self.blocks)
+        full_packet = full_packet.encode("utf-8")
+
+        if encrypted:
+            full_packet = self.xor_encrypt(packet=full_packet)
+
+        return full_packet
