60a96d48f3c7a59d8ae38b2cd84bf5acaec460b5
Santi GM <s.g.martin94@gmail.com>
2022-12-26 13:22:14 +0100

    Add black formatting

--- constants/network_constants.py
+++ constants/network_constants.py
@@ -1,4 +1,3 @@
-
 class InternalKeys:
     XOR_SEND = 0x23
     XOR_RECIEVE = 0xA3
@@ -6,8 +5,7 @@ class InternalKeys:
 
 class Ports:
     INTERNAL = 5012
-    AUTH_CLIENT = 5330,
-    GAME_CLIENT = 5340,
-    UDP1 = 5350,
+    AUTH_CLIENT = (5330,)
+    GAME_CLIENT = (5340,)
+    UDP1 = (5350,)
     UDP2 = 5351
-
