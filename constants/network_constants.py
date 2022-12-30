866184156501df8b08121a81b16e936f4cc0798d
Santiago Garcia <s.g.martin94@gmail.com>
2022-12-30 16:33:58 +0100

    Add server types enum

--- constants/network_constants.py
+++ constants/network_constants.py
@@ -9,3 +9,17 @@ class Ports:
     GAME_CLIENT = 5340
     UDP1 = 5350
     UDP2 = 5351
+
+# A user with DEV rights will see every type of server.
+# Admins and regular users will only see Entire/Adult types.
+# Everyone will see trainee type servers
+
+class ServerTypes:
+    ENTIRE = 0
+    ADULT = 1
+    CLAN = 2
+    EMPTY = 3
+    TEST = 4
+    DEVELOPMENT = 5
+    NONE = 6
+    TRAINEE = 21
