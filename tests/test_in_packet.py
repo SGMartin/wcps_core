2f18b68241aba02054c8a023939656421bd6dc73
SGMartin <s.g.martin94@gmail.com>
2024-07-27 22:44:04 +0200

    Fix unit tests

--- tests/test_in_packet.py
+++ tests/test_in_packet.py
@@ -7,7 +7,7 @@ from wcps_core.packets import InPacket
 class TestInPacket(unittest.TestCase):
     def setUp(self):
         self.buffer = bytearray(b'\xf2\xf6\xf4\xf6\xf1\xf2\xf4\xf0\xe3\xf7\xf2\xf2\xf1\xe3\xc9')
-        self.packet = InPacket(self.buffer, xor_key=0xC3)
+        self.packet = InPacket(self.buffer, self, xor_key=0xC3)
 
     def test_xor_decrypt(self):
         # Test decryption with a different key
