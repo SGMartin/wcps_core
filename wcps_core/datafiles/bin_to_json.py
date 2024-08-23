4d5d95b2253f2ae747c808511677127da03499e7
SGMartin <s.g.martin94@gmail.com>
2024-08-23 17:43:52 +0200

    Add vehicle data export

--- wcps_core/datafiles/bin_to_json.py
+++ wcps_core/datafiles/bin_to_json.py
@@ -1,40 +1,56 @@
 import re
 import json
 
+
 def parse_section(content, start_tag, end_tag):
-    pattern = re.compile(rf'{start_tag}(.*?){end_tag}', re.DOTALL)
+    pattern = re.compile(rf"{start_tag}(.*?){end_tag}", re.DOTALL)
     matches = pattern.findall(content)
     return matches
 
+
 def parse_block(block):
     result = {}
-    block = re.sub(r'<!--|//-->', '', block).strip()
-    sections = re.findall(r'<(.*?)>(.*?)</\1>', block, re.DOTALL)
+    block = re.sub(r"<!--|//-->", "", block).strip()
+    sections = re.findall(r"<(.*?)>(.*?)</\1>", block, re.DOTALL)
+
     for section, content in sections:
-        items = re.findall(r'(\w+)\s+=\s+(.*)', content.strip())
-        result[section] = {key: value.strip() for key, value in items}
+        nested_sections = re.findall(r"<(.*?)>(.*?)</\1>", content.strip(), re.DOTALL)
+
+        if nested_sections:
+            # If there are nested sections, process them recursively
+            nested_result = {}
+            for nested_section, nested_content in nested_sections:
+                items = re.findall(r"(\w+)\s+=\s+(.*)", nested_content.strip())
+                nested_result[nested_section] = {key: value.strip() for key, value in items}
+            result[section] = nested_result
+        else:
+            # No nested sections, process the current section
+            items = re.findall(r"(\w+)\s+=\s+(.*)", content.strip())
+            result[section] = {key: value.strip() for key, value in items}
+
     return result
 
+
 def parse_item_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         content = file.read()
 
     item_data = {
-        'RESOURCE': [],
-        'CHARACTER': [],
-        'ETC': [],
-        'WEAPON': [],
-        'EQUIPMENT': [],
-        'E_WEAPON': []
+        "RESOURCE": [],
+        "CHARACTER": [],
+        "ETC": [],
+        "WEAPON": [],
+        "EQUIPMENT": [],
+        "E_WEAPON": []
     }
 
     for item_type in item_data.keys():
-        start_tag = rf'\[{item_type}\]'
-        end_tag = rf'\[/{item_type}\]'
+        start_tag = rf"\[{item_type}\]"
+        end_tag = rf"\[/{item_type}\]"
         sections = parse_section(content, start_tag, end_tag)
 
         for section in sections:
-            blocks = re.findall(r'<!--(.*?)//-->', section, re.DOTALL)
+            blocks = re.findall(r"<!--(.*?)//-->", section, re.DOTALL)
             for block in blocks:
                 parsed_block = parse_block(block)
                 item_data[item_type].append(parsed_block)
@@ -43,10 +59,10 @@ def parse_item_data(file_path):
 
 
 def save_json(data, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
-file_path = 'items.bin.decoded'
+file_path = "output/decoded/items.bin.decoded"
 parsed_data = parse_item_data(file_path)
-save_json(parsed_data, 'item_data.json')
+save_json(parsed_data, "item_data.json")
--- wcps_core/datafiles/json_to_csv.py
+++ wcps_core/datafiles/json_to_csv.py
@@ -5,46 +5,46 @@ import os
 
 
 def generate_items_table(json_data, output_csv_file):
-    with open(output_csv_file, mode='w', newline='', encoding='utf-8') as file:
+    with open(output_csv_file, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(['code', 'english', 'active', 'source'])
+        writer.writerow(["code", "english", "active", "source"])
 
-        for key in ['RESOURCE', 'CHARACTER', 'ETC', 'WEAPON', 'E_WEAPON']:
+        for key in ["RESOURCE", "CHARACTER", "ETC", "WEAPON", "E_WEAPON"]:
             if key in json_data:
                 for item in json_data[key]:
-                    basic_info = item.get('BASIC_INFO', {})
-                    code = basic_info.get('CODE', '')
-                    english = basic_info.get('ENGLISH', '')
-                    active = basic_info.get('ACTIVE', '')
+                    basic_info = item.get("BASIC_INFO", {})
+                    code = basic_info.get("CODE", "")
+                    english = basic_info.get("ENGLISH", "")
+                    active = basic_info.get("ACTIVE", "")
                     writer.writerow([code, english, active, key])
 
 
 def generate_item_shop_table(json_data, output_csv_file):
-    with open(output_csv_file, mode='w', newline='', encoding='utf-8') as file:
+    with open(output_csv_file, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow([
-            'code', 'is_buyable',
-            'buy_type', 'buy_option',
-            'cost', 'required_bp',
-            'required_level',
-            'required_premium'
+            "code", "is_buyable",
+            "buy_type", "buy_option",
+            "cost", "required_bp",
+            "required_level",
+            "required_premium"
             ]
             )
 
-        for key in ['RESOURCE', 'CHARACTER', 'ETC', 'WEAPON', 'E_WEAPON']:
+        for key in ["RESOURCE", "CHARACTER", "ETC", "WEAPON", "E_WEAPON"]:
             if key in json_data:
                 for item in json_data[key]:
-                    basic_info = item.get('BASIC_INFO', {})
-                    buy_info = item.get('BUY_INFO', {})
-                    buy2_info = item.get('USE_INFO', {})
-                    code = basic_info.get('CODE', '')
-                    is_buyable = buy_info.get('BUYABLE', '')
-                    buy_type = buy_info.get('BUYTYPE', '')
-                    buy_option = buy_info.get('BUYOPTION', '')
-                    cost = buy_info.get('COST', '')
-                    required_bp = buy_info.get('REQ_BP', '')
-                    required_level = buy_info.get('REQ_LVL', '')
-                    required_premium = buy2_info.get('APPLY_TARGET', '')
+                    basic_info = item.get("BASIC_INFO", {})
+                    buy_info = item.get("BUY_INFO", {})
+                    buy2_info = item.get("USE_INFO", {})
+                    code = basic_info.get("CODE", "")
+                    is_buyable = buy_info.get("BUYABLE", "")
+                    buy_type = buy_info.get("BUYTYPE", "")
+                    buy_option = buy_info.get("BUYOPTION", "")
+                    cost = buy_info.get("COST", "")
+                    required_bp = buy_info.get("REQ_BP", "")
+                    required_level = buy_info.get("REQ_LVL", "")
+                    required_premium = buy2_info.get("APPLY_TARGET", "")
                     writer.writerow([
                         code, is_buyable,
                         buy_type, buy_option,
@@ -55,31 +55,100 @@ def generate_item_shop_table(json_data, output_csv_file):
 
 
 def generate_weapon_damage_table(json_data, output_csv_file):
-    with open(output_csv_file, mode='w', newline='', encoding='utf-8') as file:
+    with open(output_csv_file, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(['code', 'power', 'personal', 'surface', 'air', 'ship'])
+        writer.writerow(["code", "power", "personal", "surface", "air", "ship"])
 
-        if 'WEAPON' in json_data:
-            for weapon in json_data['WEAPON']:
-                basic_info = weapon.get('BASIC_INFO', {})
-                code = basic_info.get('CODE', '')
+        if "WEAPON" in json_data:
+            for weapon in json_data["WEAPON"]:
+                basic_info = weapon.get("BASIC_INFO", {})
+                code = basic_info.get("CODE", "")
 
-                ability_info = weapon.get('ABILITY_INFO', {})
-                power = ability_info.get('POWER', '')
+                ability_info = weapon.get("ABILITY_INFO", {})
+                power = ability_info.get("POWER", "")
 
-                target_info = weapon.get('TARGET_INFO', {})
-                personal = target_info.get('PERSONAL', '')
-                surface = target_info.get('SURFACE', '')
-                air = target_info.get('AIR', '')
-                ship = target_info.get('SHIP', '')
+                target_info = weapon.get("TARGET_INFO", {})
+                personal = target_info.get("PERSONAL", "")
+                surface = target_info.get("SURFACE", "")
+                air = target_info.get("AIR", "")
+                ship = target_info.get("SHIP", "")
 
                 writer.writerow([code, power, personal, surface, air, ship])
 
 
+def generate_vehicle_weapon_damage_table(json_data, output_csv_file):
+    with open(output_csv_file, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["name", "code", "power", "personal", "surface", "air", "ship"])
+
+        if "E_WEAPON" in json_data:
+            for weapon in json_data["E_WEAPON"]:
+                basic_info = weapon.get("BASIC_INFO", {})
+                name = basic_info.get("ENGLISH", "")
+                code = basic_info.get("CODE", "")
+
+                ability_info = weapon.get("ABILITY_INFO", {})
+                power = ability_info.get("POWER", "")
+
+                target_info = weapon.get("TARGET_INFO", {})
+                personal = target_info.get("PERSONAL", "")
+                surface = target_info.get("SURFACE", "")
+                air = target_info.get("AIR", "")
+                ship = target_info.get("SHIP", "")
+
+                writer.writerow([name, code, power, personal, surface, air, ship])
+
+
+def generate_vehicle_table(json_data, output_csv_file):
+    with open(output_csv_file, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "name",
+            "code",
+            "health",
+            "seat_id",
+            "weapon_code",
+            "weapon_ammo",
+            "weapon_magazine",
+            "armor_type"
+            ]
+            )
+
+        if "EQUIPMENT" in json_data:
+            for vehicle in json_data["EQUIPMENT"]:
+                basic_info = vehicle.get("BASIC_INFO", {})
+                name = basic_info.get("ENGLISH", "")
+                code = basic_info.get("CODE", "")
+
+                ability_info = vehicle.get("ABILITY_INFO", {})
+                vehicle_health = ability_info.get("DEFENCE", 0)
+
+                seats = vehicle.get("RIDING_INFO", {})
+
+                for seat in seats.values():
+                    print(seat)
+                    seat_id = seat.get("SEAT_NUMBER", "")
+                    weapon_code = seat.get("CODE", "")
+                    weapon_ammo = seat.get("AMMONNUM", 0)
+                    weapon_magazine = seat.get("MAGAZINENUM", 0)
+                    armor_type = seat.get("ARMOR_TYPE", 0)
+
+                    writer.writerow([
+                        name,
+                        code,
+                        vehicle_health,
+                        seat_id,
+                        weapon_code,
+                        weapon_ammo,
+                        weapon_magazine,
+                        armor_type]
+                    )
+
+
 def main():
     # Set up argument parser
-    parser = argparse.ArgumentParser(description='Generate CSV files from JSON data.')
-    parser.add_argument('json_file', type=str, help='Path to the input JSON file')
+    parser = argparse.ArgumentParser(description="Generate CSV files from JSON data.")
+    parser.add_argument("json_file", type=str, help="Path to the input JSON file")
     args = parser.parse_args()
 
     json_file_path = args.json_file
@@ -89,23 +158,28 @@ def main():
 
     # Determine output file paths
     base_dir = os.path.dirname(json_file_path)
-    items_table_path = os.path.join(base_dir, 'items.csv')
-    item_shop_table_path = os.path.join(base_dir, 'item_shop.csv')
-    weapon_damage_table_path = os.path.join(base_dir, 'weapon_damage.csv')
+    items_table_path = os.path.join(base_dir, "items.csv")
+    item_shop_table_path = os.path.join(base_dir, "item_shop.csv")
+    weapon_damage_table_path = os.path.join(base_dir, "weapon_damage.csv")
+    e_weapons_table_path = os.path.join(base_dir, "vehicle_weapon_damage.csv")
+    vehicle_table_path = os.path.join(base_dir, "vehicles.csv")
 
     # Load JSON data
-    with open(json_file_path, 'r', encoding='utf-8') as file:
+    with open(json_file_path, "r", encoding="utf-8") as file:
         json_data = json.load(file)
 
     # Generate the CSV tables
     generate_items_table(json_data, items_table_path)
     generate_item_shop_table(json_data, item_shop_table_path)
     generate_weapon_damage_table(json_data, weapon_damage_table_path)
+    generate_vehicle_weapon_damage_table(json_data, e_weapons_table_path)
+    generate_vehicle_table(json_data, vehicle_table_path)
 
     print("CSV files generated:")
     print(f"- {items_table_path}")
     print(f"- {item_shop_table_path}")
     print(f"- {weapon_damage_table_path}")
+    print(f"- {vehicle_table_path}")
 
 
 if __name__ == "__main__":
