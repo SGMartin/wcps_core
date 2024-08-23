e9e8e0d23827a8cf348a2b7783afe73dd51367e7
SGMartin <s.g.martin94@gmail.com>
2024-08-23 16:27:35 +0200

    Add vehicle spawn parser

--- wcps_core/datafiles/map_xml_reader.py
+++ wcps_core/datafiles/map_xml_reader.py
@@ -16,18 +16,12 @@ def parse_map_list(xml_string):
     # Iterate through each map in the XML
     for map_element in root.findall("Map"):
         map_name = map_element.get("Name")
-        maps.append({
-            "name": map_name,
-            "active": True
-        })
+        maps.append({"name": map_name, "active": True})
 
     # Check for the inactive maps and update the "active" field
     for map_element in root.findall("_Map"):
         map_name = map_element.get("Name")
-        maps.append({
-            "name": map_name,
-            "active": False
-        })
+        maps.append({"name": map_name, "active": False})
 
     # Convert the list to a JSON formatted string
     return json.dumps(maps, indent=4)
@@ -35,7 +29,7 @@ def parse_map_list(xml_string):
 
 def parse_map_info(xml_string):
     # Remove the unclosed <Audio> tag
-    xml_string = re.sub(r'<Audio\b[^>]*>', '', xml_string)
+    xml_string = re.sub(r"<Audio\b[^>]*>", "", xml_string)
 
     # Parse the XML string
     root = ET.fromstring(xml_string)
@@ -59,7 +53,7 @@ def parse_map_info(xml_string):
         map_info["Channel"] = {
             "Mission": channel_element.get("Mission"),
             "Medium": channel_element.get("Medium"),
-            "Large": channel_element.get("Large")
+            "Large": channel_element.get("Large"),
         }
 
     # Parse GameMode information
@@ -85,7 +79,10 @@ def parse_map_info(xml_string):
 
 def parse_control_points(control_point_file_stream):
     entities = control_point_file_stream.strip().split("\n\n")
-    team_dict = {0: [], 1: []}  # Initialize the dictionary for team 0 (DERB) and team 1 (NIU)
+    team_dict = {
+        0: [],
+        1: [],
+    }  # Initialize the dictionary for team 0 (DERB) and team 1 (NIU)
     entity_count = len(entities)  # Count the number of entities
 
     for idx, entity in enumerate(entities):
@@ -101,6 +98,32 @@ def parse_control_points(control_point_file_stream):
             team_dict[team].append(idx)
 
     return entity_count, team_dict
+
+
+def parse_vehicles(vehicle_data_stream):
+    entities = vehicle_data_stream.strip().split("\n\n")
+    vehicles = {}
+
+    for idx, entity in enumerate(entities):
+        lines = entity.splitlines()
+        for line in lines:
+            if line.startswith("ObjectSpawn.Code"):
+                vehicle_code = line.split()[-1]
+            elif line.startswith("ObjectSpawn.SpawnInterval"):
+                spawn_interval = int(line.split()[-1])
+            elif line.startswith("ObjectSpawn.Position"):
+                raw_coords = line.split()[-1]
+                full_coords = [float(coord) for coord in raw_coords.split("/")]
+
+        # If team is not -1, add the entity ID to the dictionary
+        if vehicle_code is not None:
+            vehicles[idx] = {
+                "code": vehicle_code,
+                "coords": full_coords,
+                "spawn_interval": spawn_interval,
+            }
+
+    return vehicles
 
 
 def parse_map_settings_to_csv(input_json, mapper):
@@ -124,13 +147,15 @@ def parse_map_settings_to_csv(input_json, mapper):
         "premium",
         "active",
         "flags",
-        "spawn_flags"
-        ]
+        "spawn_flags",
+    ]
 
     # Prepare the data for CSV writing
     csv_data = []
     for item in data:
-        spawn_flags = ",".join([str(value[0]) for value in item["spawn_flags"].values()])
+        spawn_flags = ",".join(
+            [str(value[0]) for value in item["spawn_flags"].values()]
+        )
         row = {
             "map_id": item["Identity"],
             "map_name": mapper.get(item["Identity"], "unknown"),
@@ -144,17 +169,45 @@ def parse_map_settings_to_csv(input_json, mapper):
             "premium": item["Restriction"],
             "active": True,
             "flags": item["flags"],
-            "spawn_flags": spawn_flags
+            "spawn_flags": spawn_flags,
         }
         csv_data.append(row)
 
     # Write the data to the CSV file
-    with open(csv_file_path, 'w', newline='') as file:
+    with open(csv_file_path, "w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=csv_columns)
         writer.writeheader()
         writer.writerows(csv_data)
 
     print(f"Map settings saved to CSV: {csv_file_path}")
+
+
+def parse_vehicles_to_csv(data, filename):
+    """
+    Writes the nested dictionary to a CSV file.
+
+    Parameters:
+    - data (dict): The nested dictionary containing the data.
+    - filename (str): The name of the CSV file to write to.
+    """
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.writer(file)
+
+        # Write the header row
+        writer.writerow(["map_id", "vehicle_id", "vehicle_code", "coords", "spawn_interval"])
+
+        # Iterate over the top-level dictionary
+        for map_id, nested_dict in data.items():
+            # Iterate over the nested dictionary
+            for vehicle_id, entry in nested_dict.items():
+                vehicle_code = entry.get("code", "")
+                coords = entry.get("coords", "")
+                spawn_interval = entry.get("spawn_interval", "")
+
+                # Write the row to the CSV file
+                writer.writerow([map_id, vehicle_id, vehicle_code, coords, spawn_interval])
+
+    print("Exported vehicles to map vehicle table")
 
 
 def main(input_directory):
@@ -165,23 +218,40 @@ def main(input_directory):
 
     # Get all of the map folders
     map_folders = [
-        name for name
-        in os.listdir(input_directory)
+        name
+        for name in os.listdir(input_directory)
         if os.path.isdir(os.path.join(input_directory, name))
-        ]
+    ]
 
     map_dict = {map_folder.lower(): map_folder for map_folder in map_folders}
 
     all_map_settings = []
     id_name_mapper = {}
+    all_vehicles = {}
+
     for listed_map in json.loads(map_list_json):
         this_map = listed_map["name"]
+        vehicle_file = os.path.join(
+            input_directory,
+            map_dict[this_map.lower()],
+            "ObjectSpawnTemplate.dat"
+            )
+        has_vehicles = os.path.isfile(vehicle_file)
 
         if this_map.lower() in map_dict.keys():
             with open(
-                file=os.path.join(input_directory, map_dict[this_map.lower()], "MapInfo.xml"),
-                mode="r"
-            ) as map_data, open(file=os.path.join(input_directory, map_dict[this_map.lower()], "ControlPointTemplate.dat"), mode="r") as spawn_points:
+                file=os.path.join(
+                    input_directory, map_dict[this_map.lower()], "MapInfo.xml"
+                ),
+                mode="r",
+            ) as map_data, open(
+                file=os.path.join(
+                    input_directory,
+                    map_dict[this_map.lower()],
+                    "ControlPointTemplate.dat",
+                ),
+                mode="r",
+            ) as spawn_points:
                 map_data_file = map_data.read()
                 # Load main xml settings
                 map_data_json = parse_map_info(map_data_file)
@@ -191,6 +261,14 @@ def main(input_directory):
                 # Append them to map data
                 map_data_json["flags"] = map_spawn_points[0]
                 map_data_json["spawn_flags"] = map_spawn_points[1]
+
+                # Parse vehicles if needed
+                if has_vehicles:
+                    with open(vehicle_file, "r") as vehicle_stream:
+                        raw_vehicles = vehicle_stream.read()
+                        vehicles = parse_vehicles(raw_vehicles)
+                        all_vehicles[int(map_data_json["Identity"])] = vehicles
+
                 # Append to map settings
                 map_id = map_data_json["Identity"]
                 id_name_mapper[map_id] = this_map.lower()
@@ -209,6 +287,9 @@ def main(input_directory):
 
     # Export to csv
     parse_map_settings_to_csv(json.dumps(all_map_settings, indent=4), id_name_mapper)
+
+    # Generate vehicle table
+    parse_vehicles_to_csv(all_vehicles, filename="output/csv/map_vehicles.csv")
 
 
 if __name__ == "__main__":
